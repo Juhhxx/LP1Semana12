74a5060b05b71d1d652c367cc09d37af59a7b439
Julia Costa <juliacosta232005@gmail.com>
2024-05-30 21:21:44 +0100

    Finnish project Palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,36 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"{arg} -> {VerifyPalindrome(arg)}");
+            }
+        }
+        public static bool VerifyPalindrome(string text)
+        {
+            bool Palindrome(string text)
+            {
+                string _invertText = "";
+                int _lenText = text.Length;
+
+                for (int i = _lenText; i > 0; i--)
+                {
+                    _invertText += text[i-1];
+                }
+
+                return text == _invertText;
+            }
+
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (text.Length < 2)
+            {
+                return true;
+            }
+            else 
+                return Palindrome(text);
         }
     }
 }
