fa4b20bd6c214d8ed3c084310a347962b06f37ab
Julia Costa <juliacosta232005@gmail.com>
2024-05-30 22:02:21 +0100

    Add MinMaxStruct

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -12,5 +12,17 @@ namespace MultipleStuff
             max = this.Max();
             return this.Min();
         }
+        
+        public struct MinMaxStruct
+        {
+            public double Min { get; }
+            public double Max { get; }
+
+            public MinMaxStruct(double min, double max)
+            {
+                Min = min;
+                Max = max;
+            }
+        }
     }
 }
